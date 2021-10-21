cc6b44d3c2169ec424bfe0d8e939de171e5e5fe5
kt <peacebyte@hotmail.com>
2021-10-21 20:58:09 +0800

    add KTAnalytics component

---
+++ KTAnalytics/AnalysisManager.swift
@@ -0,0 +1,46 @@
+//
+//  AnalysisManager.swift
+//  photo retouch
+//
+//  Created by Hi on 2020/10/7.
+//  Copyright © 2020 magician. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import KTFoundationKit
+
+
+
+
+
+
+
+
+
+@objc public class AnalysisManager: NSObject {
+   
+    
+    private override init() {
+        super.init()
+    }
+    
+    @objc public static let sharedInstance: AnalysisManager = {
+        AnalysisManager()
+    }()
+    
+    
+    @objc public func startService(){
+        startFirebaseService()
+    }
+    
+    private func startFirebaseService(){
+        
+        LogManager.debug("started")
+        FirebaseApp.configure()
+        LogManager.debug("finished")
+       
+    }
+    
+   
+}
